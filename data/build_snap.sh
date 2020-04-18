19e5b7f926a647598f37681020e996a7d633bc15
jopohl <Johannes.Pohl90@gmail.com>
2020-04-18 17:18:26 +0200

    update script

--- data/build_snap.sh
+++ data/build_snap.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+DIR=$(dirname "$(readlink -f "$0")")
+
+echo "Copy snapcraft.yaml to base directory"
+cd $DIR
+cp snapcraft.yaml ..
+cd ..
+
 SNAPCRAFT_BUILD_ENVIRONMENT_CPU=4 SNAPCRAFT_BUILD_ENVIRONMENT_MEMORY=5G snapcraft
 sudo snap install --dangerous urh*.snap
 urh
