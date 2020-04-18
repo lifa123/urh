5aef441b86a19552617dd2b06e77fae5727b47cd
jopohl <Johannes.Pohl90@gmail.com>
2020-04-18 18:29:46 +0200

    update version

--- data/build_snap.sh
+++ data/build_snap.sh
@@ -6,6 +6,8 @@ echo "Copy snapcraft.yaml to base directory"
 cd $DIR
 cp snapcraft.yaml ..
 cd ..
+sed -i "s/version\: git/version\: $(python3 src/urh/version.py)/" snapcraft.yaml
+
 
 SNAPCRAFT_BUILD_ENVIRONMENT_CPU=4 SNAPCRAFT_BUILD_ENVIRONMENT_MEMORY=5G snapcraft
 sudo snap install --dangerous urh*.snap
